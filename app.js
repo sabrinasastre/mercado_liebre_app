a22ccef96f60a214a8597ccab0a22ded8c0fcdd2
sabrinadanielasastre@gmail.com <sabrinadanielasastre@gmail.com>
2021-12-30 21:10:02 -0300

    1st commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require ("express");
+const path = require ("path");
+
+const app = express ();
+
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use (express.static(publicPath));
+
+app.listen (3000, ()=> {
+    console.log ("servidor corriendo en puerto 3000")
+});
+
+app.get ("/", (req, res)=>{
+    res.sendFile(path.resolve (__dirname, "./views/home.html"))
+});
+
+
+
+
