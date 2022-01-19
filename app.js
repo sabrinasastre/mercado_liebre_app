a99e7d40be7de01a0b3acd7949077942a86b8e0c
sabrinadanielasastre@gmail.com <sabrinadanielasastre@gmail.com>
2022-01-18 21:56:42 -0300

    configuraciones para Heroku, Procfile y app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express ();
 const publicPath = path.resolve(__dirname, "./public");
 app.use (express.static(publicPath));
 
-app.listen (3000, ()=> {
+app.listen (process.env.PORT || 3000, ()=> {
     console.log ("servidor corriendo en puerto 3000")
 });
 
@@ -23,6 +23,9 @@ app.get ("/register", (req, res)=>{
     res.sendFile(path.resolve (__dirname, "./views/register.html"))
 });
 
+app.get("*", (req,res)=>{
+    res.send("¡la página que estás buscando, no se encuentra!")
+});
 
 
 
