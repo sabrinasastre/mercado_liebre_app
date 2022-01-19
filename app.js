8928bd6157c3b131fea923c7a3986e90d8fd091b
sabrinadanielasastre@gmail.com <sabrinadanielasastre@gmail.com>
2022-01-18 22:23:49 -0300

    borré punto y coma

--- app.js
+++ app.js
@@ -9,23 +9,23 @@ app.use (express.static(publicPath));
 
 app.listen (process.env.PORT || 3000, ()=> {
     console.log ("servidor corriendo en puerto 3000")
-});
+})
 
 app.get ("/", (req, res)=>{
     res.sendFile(path.resolve (__dirname, "./views/home.html"))
-});
+})
 
 app.get ("/login", (req, res)=>{
     res.sendFile(path.resolve (__dirname, "./views/login.html"))
-});
+})
 
 app.get ("/register", (req, res)=>{
     res.sendFile(path.resolve (__dirname, "./views/register.html"))
-});
+})
 
 app.get("*", (req,res)=>{
     res.send("¡la página que estás buscando, no se encuentra!")
-});
+})
 
 
 
